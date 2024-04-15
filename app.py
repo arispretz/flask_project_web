327cffe5eef1c5a5da5b0988ac067fda2328262f
arispretz <arispretz@gmail.com>
2024-04-15 05:21:28 -0300

    other one

--- app.py
+++ app.py
@@ -389,7 +389,7 @@ def update_feedback(feedback_id):
 if __name__ == '__main__':
     # for deployment
     # to make it work for both production and development
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5500))
     app.run(debug=True, host='0.0.0.0', port=port)
     
     
